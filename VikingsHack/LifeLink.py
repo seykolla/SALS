6701536933be378962c335f14c3b250fc6ad6a6f
Sey Kolla <shreyakolla@gmail.com>
2024-02-03 17:27:30 -0800

    hsjdflksjdf

--- VikingsHack/LifeLink.py
+++ VikingsHack/LifeLink.py
@@ -3,14 +3,14 @@ import responses
 import random
 
 class LifeLink:
+
     def __init__(self, distance, service):
-        assert service == "urgent care" or service =="vet"
-        assert type(distance)== float or type(distance) == int
+        with open('initialize.txt', 'r') as f:
+            self.distance = float(f.readline()[0:2]) * 1.60934 *1000
+            service = f.readline()
 
         self.client = googlemaps.Client(key= 'AIzaSyDZP67HsK_6GmGq3WaL4IIZOeHIbHgg-hk')
         self.location = self.client.geolocate()['location']
-        self.distance = distance * 1.60934 *1000
-
         if(service == 'vet'):
             self.service = 'veterinary services'
         else:
@@ -25,6 +25,7 @@ class LifeLink:
     def get_service(self):
         return self.service
     
+    
     def get_nearby_places(self):
         nearby_places = self.client.places_nearby(keyword= self.service, location =self.location, radius = self.distance)
         if(nearby_places['results'] is None):
@@ -38,36 +39,11 @@ class LifeLink:
                 results.append({'name':i['name'], 'address': i['vicinity'], 'distance': {distance}, 'wait_time': wait_time, 'open': i['opening_hours']['open_now']})
             return results
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-with open('instance.txt', 'r') as f:
-    distance = float(f.readline(1))
-    service = f.readline(2)
 
 
-instace = LifeLink(distance, service)
-=======
-=======
->>>>>>> 92d2a9554c532f5fb1afc772d69ffd14b09deac3
-f = open('initialize.txt', 'r')
-instance = LifeLink(f.readline().rstrip()[0:2], 'vet')
-places = instance.get_nearby_places()
-marker = instance.get_current_location()
-with open('location.txt', 'w') as f:
-    f.write(str(list(places)) + "\n")
-    f.write(str(list(marker)))
-    f.close()
-<<<<<<< HEAD
->>>>>>> ed8d18a788eeb88aecadf39d297d95a32616cd92
-=======
-=======
-
-with open('instance.txt', 'r') as f:
-    distance = float(f.readline(1))
-    service = f.readline(2)
 
 
-instace = LifeLink(distance, service)
->>>>>>> 3512f91 (hsjdflksjdf)
->>>>>>> 92d2a9554c532f5fb1afc772d69ffd14b09deac3
+
+
+
+
